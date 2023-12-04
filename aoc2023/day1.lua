16db438f08b3466bfc70babdc55880fdb6825f50
snipchu <pipandw@gmail.com>
2023-12-04 12:52:43 -0600

    aoc2023: d1p2

--- aoc2023/day1.lua
+++ aoc2023/day1.lua
@@ -1,11 +1,17 @@
 local file = io.open("day1.txt", "r")
 file = file:read("*all")
 local numbers = {}
--- local wordnums = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero" }
+local wordnums = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" }
 local sum = 0
 
 for line in file.gmatch(file, "%S+") do
-	-- line = string.gsub(line, "zero", "0")
+	for i = 1, #line do
+		for x in pairs(wordnums) do
+			if string.find(line:sub(1, i), wordnums[x]) ~= nil then
+				line = string.gsub(line, wordnums[x]:sub(1, -2), x - 1)
+			end
+		end
+	end
 	local numline = line:gsub("%D+", "")
 	numline = string.sub(numline, 1, 1) .. string.sub(numline, -1)
 	table.insert(numbers, numline)
