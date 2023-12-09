3965afc37e9737669140cd1d43fb410e7f1d3fec
snipchu <pipandw@gmail.com>
2023-12-09 00:31:14 -0600

    aoc2023: d9p1

---
+++ aoc2023/day9.py
@@ -0,0 +1,26 @@
+import re
+file = open("./day9.txt", "r").read().splitlines()
+
+def newsequence(oldsequence):
+    list = []
+    for line in range(1,len(oldsequence)):
+        list.append(oldsequence[line]-oldsequence[line-1])
+    return list
+
+sum = 0
+for line in range(len(file)):
+    file[line] = re.split(" ", file[line])
+    file[line] = [int(i) for i in file[line]]
+    archive = [file[line], newsequence(file[line])]
+
+    while archive[-1].count(0) != len(archive[-1]):
+        archive.append(newsequence(archive[-1]))
+
+    archive[-1].append(0)
+    archive.reverse()
+
+    for line in range(1,len(archive)):
+        archive[line].append(archive[line][-1]+archive[line-1][-1])
+    sum += archive[-1][-1]
+
+print(sum)
